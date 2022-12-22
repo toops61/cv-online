5073f3a0ad3ab6506524b64be4ed1393d23109d2
Toops61 <toops61@gmail.com>
2022-12-22 17:03:23 +0100

    audio player add

--- src/composants/jsProjects/AudioPlayer.jsx
+++ src/composants/jsProjects/AudioPlayer.jsx
@@ -1,5 +1,169 @@
+import { useEffect, useState, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { updateGeneralParams } from "../../redux";
+
 export default function AudioPlayer() {
+  const [repeat, setRepeat] = useState('');
+  const [songsArray, setSongsArray] = useState([]);
+  const [play, setPlay] = useState(false);
+  const [timerAudio, setTimerAudio] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [volume, setVolume] = useState(.1);
+  const [songSelected, setSongSelected] = useState({
+    id:1,
+    url:'',
+    artist:'',
+    song:'',
+    album:'',
+    pictureURL:''
+  });
+  //const [jsmediatags, setJsmediatags] = useState('');
+  const jsmediatags = window.jsmediatags;
+
+  const getSongs = () => {
+    const importAll = r => {
+      return r.keys().map(r);
+    }
+    let songs = importAll(require.context('../../assets/songs', false, /\.(mp3|m4a|webp|wav|aiff)$/));
+    //images = images.filter(e => !e.includes('question'));
+    let array = songs.map((el,index) => {
+      return {
+        id:index+1,
+        url:el
+      }
+    })
+    setSongsArray([...array]);
+    const tempSongObject = {...songSelected,...array[0]};
+    setSongSelected({...tempSongObject});
+  }
+
+  useEffect(() => {
+    const timerPlay = setInterval(() => {
+      play && setTimerAudio(timerVideo => timerVideo+1);
+    }, 1000);
+
+    audioRef.current && (play ? audioRef.current.play() : audioRef.current.pause());
+
+    return () => {
+      clearInterval(timerPlay);
+    }
+  }, [play])
+
+  const audioRef = useRef();
+
+  const getCover = tags => {
+    const data = tags.picture.data;
+    const format = tags.picture.format;
+    let base64String = "";
+    data.map(e => base64String += String.fromCharCode(e));
+    const imageUrl = `data:${format};base64,${window.btoa(base64String)}`;
+    return imageUrl;
+  }
+
+  //check songs metadata
+const getTags = async () => {
+  const targetUrl = audioRef.current.src;
+  return await jsmediatags.read(targetUrl, {
+    onSuccess: tag => {
+      const songTags = tag.tags;
+      const tempSongObject = {
+        ...songSelected,
+        artist:songTags.artist,
+        album:songTags.album,
+        song:songTags.title,
+        pictureURL:getCover(songTags)
+      }
+      setSongSelected({...tempSongObject});
+    },
+    onError: error => console.log(error)
+  })
+}
+
+const formatTime = time => {
+  let seconds = Math.round(time%60);
+  seconds < 10 && (seconds = '0' + seconds);
+  const formatted = Math.floor(time / 60) + ':' + seconds;
+  return formatted;
+};
+
+const getDuration = () => {
+  setDuration(audioRef.current.duration);
+};
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getSongs();
+    dispatch(updateGeneralParams({ darkMode: true }));
+  }, [])
+  
+  useEffect(() => {
+    if (songsArray.length) {
+      audioRef.current.volume = volume;
+    }
+  }, [songsArray])
+
+  useEffect(() => {
+    if (audioRef.current) {
+      getTags();
+    }
+  }, [songSelected])
+  
+  const handlePlay = e => {
+    setPlay(!play);
+  }
+
+  document.querySelector(".button-container")?.classList.add("hide");
+
   return (
-    <div>AudioPlayer</div>
+    <div className="audio-player-page">
+      <main className="player-body">
+        <div className="player-back">
+          <div className="triangle-corner"></div>
+        </div>
+        <div className="player-front"></div>
+        <div className="volume-buttons">
+          <button className="volume"></button>
+          <button className="volume"></button>
+        </div>
+        {songsArray.length && <audio src={songsArray[0].url} className="song" ref={audioRef} onDurationChange={getDuration}></audio>}
+        <section className="screen-container">
+          {songSelected.pictureURL ? <img src={songSelected.pictureURL} alt="cover" /> : <div className="no-picture"></div>}
+        </section>
+        <section className="title-container">
+          <div className="title">
+            <h1>{songSelected.artist}</h1>
+            <p>{songSelected.song}</p>
+            <p>{songSelected.album}</p>
+          </div>
+          <div className="title-number"><p>{`${songSelected.id}/${songsArray.length}`}</p></div>
+        </section>
+        <section className="progressbar-container">
+          <div className="progress-bar">
+            <div className="progress"></div>
+          </div>
+          <div className="time">
+            <p>0:00</p>
+            <p>{formatTime(duration)}</p>
+          </div>
+        </section>
+        <section className="controls-container">
+          <div className="random"></div>
+          <button className="backward"></button>
+          <button className={play ? "play-pause active" : "play-pause"} onClick={handlePlay}>
+            <div className="play"></div>
+            <div className="pause"></div>
+          </button>
+          <button className="forward"></button>
+          <div className={repeat ? "repeat selected" : "repeat"}>
+            {repeat === 'one' && <p>1</p>}
+          </div>
+        </section>
+      </main>
+      <Link to="/MaulaveStephane/Projects">
+        <button className="previous-page"></button>
+      </Link>
+    </div>
   )
 }
--- src/composants/jsProjects/VideoPlayer.jsx
+++ src/composants/jsProjects/VideoPlayer.jsx
@@ -1,8 +1,11 @@
 import { useRef } from "react";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import videoShintou from '../../assets/Shintou_montage.mp4';
 
 export default function VideoPlayer() {
+  document.querySelector(".button-container")?.classList.add("hide");
+  
   const [play, setPlay] = useState(false);
   const [volume, setVolume] = useState(2);
   const [timerVideo, setTimerVideo] = useState(0);
@@ -20,14 +23,13 @@ export default function VideoPlayer() {
   const getDuration = () => {
       setDuration(videoRef.current.duration);
       videoRef.current.volume = .5;
-      document.querySelector('#scroll-time').setAttribute('max',Math.ceil(videoRef.current.duration));
   };
 
   useEffect(() => {
     const timerPlay = setInterval(() => {
       play && setTimerVideo(timerVideo => timerVideo+1);
     }, 1000);
-  
+
     videoRef.current && (play ? videoRef.current.play() : videoRef.current.pause());
 
     return () => {
@@ -36,13 +38,34 @@ export default function VideoPlayer() {
   }, [play])
   
   useEffect(() => {
+    if (timerVideo && timerVideo > duration) {
+      setPlay(false);
+      setTimerVideo(0);
+      videoRef.current.currentTime = 0;
+    }
+  }, [timerVideo])
+  
+
+  useEffect(() => {
     videoRef.current && (videoRef.current.volume = volume/10);
   }, [volume])
   
-  useEffect(() => {
-    console.log(timerVideo);
-  }, [timerVideo])
-  
+  const scrollTime = e => {
+    const value = e.target.value;
+    videoRef.current.currentTime = value;
+    setTimerVideo(value/1);
+  }
+
+  const fullScreenFunc = () => {
+    if (!document.fullscreenElement) {
+      document.querySelector('.video-container').requestFullscreen()
+      .catch((err) => {
+        alert(`Il y a eu un problème sur le mode plein écran: ${err.message} (${err.name})`);
+      });
+    } else {
+      document.exitFullscreen();
+    }
+  }
 
   return (
     <div className="video-player-page">
@@ -50,22 +73,34 @@ export default function VideoPlayer() {
         <h1>Lecteur <span>personnalisé</span></h1>
         <section className="player-container">
             <div className="video-container">
-                <video src={videoShintou}  ref={videoRef} onDurationChange={getDuration}></video>
-                <div className="controls-section">
-                    <button className={play ? "pause" : "play"} onClick={e => setPlay(!play)}></button>
-                    <button className={`volume ${volume === '0' ? 'off' : (volume > 4 ? 'high' : 'low')}`}>
-                        <input type="range" name="scroll-volume" id="scroll-volume" min="0" max="10" onChange={e => setVolume(e.target.value)} value={volume} />
-                    </button>
-                    <div className="time-container">
-                        <p className="actual-time">{formatTime(timerVideo)}</p>
-                    </div>
-                    <input type="range" name="scroll-time" id="scroll-time" min="0" />
-                    <p className="total-time">{formatTime(duration)}</p>
-                    <button className="full-screen"></button>
-                </div>
+              <div className="play-screen" onClick={e => setPlay(!play)}></div>
+              <video src={videoShintou}  ref={videoRef} onDurationChange={getDuration}></video>
+              <div className="controls-section">
+                  <button className={play ? "pause" : "play"} onClick={e => setPlay(!play)}></button>
+                  <button className={`volume ${volume === '0' ? 'off' : (volume > 4 ? 'high' : 'low')}`}>
+                      <input type="range" name="scroll-volume" id="scroll-volume" min="0" max="10" onChange={e => setVolume(e.target.value)} value={volume} />
+                  </button>
+                  <div className="time-container">
+                      <p className="actual-time">{formatTime(timerVideo)}</p>
+                  </div>
+                  <input 
+                    type="range" 
+                    name="scroll-time" 
+                    id="scroll-time" 
+                    min="0" 
+                    max={Math.ceil(duration)} 
+                    onChange={scrollTime}
+                    value={timerVideo}
+                  />
+                  <p className="total-time">{formatTime(duration)}</p>
+                  <button className="full-screen" onClick={fullScreenFunc}></button>
+              </div>
             </div>
         </section>
       </main>
+      <Link to="/MaulaveStephane/Projects">
+        <button className="previous-page"></button>
+      </Link>
     </div>
   )
 }
