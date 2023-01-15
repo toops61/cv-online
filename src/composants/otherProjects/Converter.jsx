630d8b927274a5e45a118b18397ad3a06e15700e
Toops61 <toops61@gmail.com>
2023-01-15 19:33:12 +0100

    convert responsive

--- src/composants/otherProjects/Converter.jsx
+++ src/composants/otherProjects/Converter.jsx
@@ -4,8 +4,9 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default function Converter() {
     const arrayTemperatures = ['Celsius','Fahrenheit','Kelvin'];
-    const arrayMasses = ['Picogramme','Nanogramme','Microgramme','Milligramme','Gramme','Kilogramme','Tonne','Once','Livre','Stone'];
-    const arrayDimensions = ['Picomètre','Nanomètre','Micromètre','Millimètre','Centimètre','Décimètre','Mètre','Décamètre','Hectomètre','Kilomètre','Pouce','Pied','Yard','Mile','Mile marin','Année lumière'];
+    const arrayTime = ['Picosecondes','Nanosecondes','Microsecondes','Millisecondes','Secondes','Minutes','Heures','Jour','Semaine','Mois','Année','Décennie','Siècle','Millénaire'];
+    const arrayMasses = ['Picogrammes','Nanogrammes','Microgrammes','Milligrammes','Centigrammes','Décigrammes','Grammes','Décagrammes','Hectogrammes','Kilogrammes','Onces','Livres','Stones','Tonnes'];
+    const arrayDimensions = ['Picomètres','Nanomètres','Micromètres','Millimètres','Centimètres','Décimètres','Mètres','Décamètres','Hectomètres','Kilomètres','Pouces','Pieds','Yards','Miles','Miles marins','Années-lumière'];
 
     const [arraySelected, setArraySelected] = useState([...arrayTemperatures]);
     const [type, setType] = useState('temperature');
@@ -26,6 +27,11 @@ export default function Converter() {
                 setType('temperature');
                 setInputFrom(0);
                 setInputTo(32);
+                break;
+            case 'time':
+                setArraySelected([...arrayTime]);
+                setType('time');
+                resetInputs();
                 break;
             case 'masse':
                 setArraySelected([...arrayMasses]);
@@ -122,8 +128,125 @@ export default function Converter() {
         return result;
     }
 
-    const convertMasse = () => {
-        console.log('convert masse');
+    const convertMasse = (object,direction) => {
+        const index = direction === 'toX' ? object.index : object.target_index;
+        let result = 0;
+        switch (index) {
+            case 10:
+                result = direction === 'toX' ? (object.input * 28.35) : (object.input / 28.35);
+                break;
+            case 11:
+                result = direction === 'toX' ? (object.input * 453.6) : (object.input / 453.6);
+                break;
+            case 12:
+                result = direction === 'toX' ? (object.input * 6350) : (object.input / 6350);
+                break;
+            case 13:
+                result = direction === 'toX' ? (object.input * 1e6) : (object.input / 1e6);
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+
+    //format time display
+    const timeDisplay = (time,format) => {
+        let finalDate = 0;
+        let seconds,minutes,restMinutes,hours,restHours,days,restDays,weeks,restWeeks,months,restMonths,years;
+        const mnSec = time => {
+            minutes = Math.floor(time/60).toLocaleString('fr', {minimumIntegerDigits: 2})+'mn';
+            seconds = (time%60).toLocaleString('fr', {minimumIntegerDigits: 2});
+            return minutes+seconds;
+        }
+        const hoursMin = time => {
+            hours = Math.floor(time/3600).toLocaleString('fr', {minimumIntegerDigits: 2})+'h';
+            restMinutes = time%3600;
+            return hours+mnSec(restMinutes);
+        }
+        const daysHours = time => {
+            days = Math.floor(time/86400)+'j';
+            restHours = time%86400;
+            return days+hoursMin(restHours);
+        }
+        const weeksDays = time => {
+            weeks = Math.floor(time/604800)+'se';
+            restDays = time%604800;
+            return weeks+daysHours(restDays);
+        }
+        const monthsWeeks = time => {
+            months = Math.floor(time/2.628e6)+'m';
+            restWeeks = time%2.628e6;
+            return months+weeksDays(restWeeks);
+        }
+        switch (format) {
+            case 'min':
+                minutes = Math.floor(time/60)+'mn';
+                seconds = (time%60).toLocaleString('fr', {minimumIntegerDigits: 2});
+                finalDate = minutes+seconds
+                break;
+            case 'hour':
+                hours = Math.floor(time/3600)+'h';
+                restMinutes = time%3600;
+                finalDate = hours+mnSec(restMinutes);
+                break;
+            case 'day':
+                days = Math.floor(time/86400)+'j';
+                restHours = time%86400;
+                finalDate = days+hoursMin(restHours);
+                break;
+            case 'week':
+                weeks = Math.floor(time/604800)+'se';
+                restDays = time%604800;
+                finalDate = weeks+daysHours(restDays);
+                break;
+            case 'month':
+                months = Math.floor(time/2.628e6)+'m';
+                restWeeks = time%2.628e6;
+                finalDate = months+weeksDays(restWeeks);
+                break;
+            case 'year':
+                years = Math.floor(time/3.154e7)+'a';
+                restMonths = time%3.154e7;
+                finalDate = years+monthsWeeks(restMonths);
+                break;
+            default:
+                break;
+        }
+        return finalDate;
+    }
+
+    const convertTime = (object,direction) => {
+        const index = direction === 'toX' ? object.index : object.target_index;
+        let result = 0;
+        if (index <= 4) {
+            const indice = direction === 'toX' ? (index - 4) : (4 - index);
+            result = object.input * (10**(3*indice));
+        } else {
+            switch (index) {
+                case 5:
+                    result = direction === 'toX' ? (object.input * 60) : timeDisplay(object.input,'min');
+                    break;
+                case 6:
+                    result = direction === 'toX' ? (object.input * 3600) : timeDisplay(object.input,'hour');
+                    break;
+                case 7:
+                    result = direction === 'toX' ? (object.input * 86400) : timeDisplay(object.input,'day');
+                    break;
+                case 8:
+                    result = direction === 'toX' ? (object.input * 604800) : timeDisplay(object.input,'week');
+                    break;
+                case 9:
+                    result = direction === 'toX' ? (object.input * 2.628e6) : timeDisplay(object.input,'month');
+                    break;
+                case 10:
+                    result = direction === 'toX' ? (object.input * 3.154e7) : timeDisplay(object.input,'year');
+                    break;
+                default:
+                    break;
+            }
+        }
+        return result;
     }
 
     const convertFunct = (id,value) => {
@@ -153,11 +276,15 @@ export default function Converter() {
                     const meters = tempObject.index <= 9 ? picoKilo(tempObject,'toX') : convertDimensions(tempObject,'toX');
                     result = tempObject.target_index <= 9 ? picoKilo({...tempObject,input:meters},'fromX') : convertDimensions({...tempObject,input:meters},'fromX');
                     break;
+                case 'time':
+                    const seconds = convertTime(tempObject,'toX');
+                    result = convertTime({...tempObject,input:seconds},'fromX');
+                    break;
                 default:
                     break;
             }
         }
-        result > 1e6 && (result = result.toExponential());
+        if (!isNaN(result) && result > 1e6) {result = (result/1).toExponential()};
         id === 'before' ? setInputTo(result) : setInputFrom(result);
     }
 
@@ -165,7 +292,7 @@ export default function Converter() {
         const id = e.target.id;
         const value = e.target.value;
         id === 'before' ? setInputFrom(value) : setInputTo(value);
-        convertFunct(id,value);
+        !isNaN(value) && convertFunct(id,value);
     }
 
     const changeUnities = e => {
@@ -174,7 +301,7 @@ export default function Converter() {
     }
 
     useEffect(() => {
-      convertFunct('before',inputFrom);
+      !isNaN(inputFrom) && convertFunct('before',inputFrom);
     }, [selectedFrom,selectedTo])
 
     
@@ -184,13 +311,13 @@ export default function Converter() {
             <h1>Convertisseurs</h1>
             <select name="type" id="type" onChange={selectType}>
                 <option value="temperature">Température</option>
+                <option value="time">Temps</option>
                 <option value="masse">Masses</option>
                 <option value="dimensions">Longueurs</option>
             </select>
             <section className="containers">
                 <div className="container">
-                    <label htmlFor="before">{selectedFrom}</label>
-                    <input type="number" name="before" id="before" onChange={handleInput} value={inputFrom} placeholder='0' />
+                    <input type="text" name="before" id="before" onChange={handleInput} value={inputFrom} placeholder='0' />
                     <select name="from" id="from" onChange={changeUnities} value={selectedFrom}>
                       {arraySelected.map(el => {
                         return (
@@ -201,8 +328,7 @@ export default function Converter() {
                 </div>
                 <div className="convert-arrow"></div>
                 <div className="container">
-                    <label htmlFor="after">{selectedTo}</label>
-                    <input type="number" name="after" id="after"  onChange={handleInput} value={inputTo} placeholder='0' />
+                    <input type="text" name="after" id="after"  onChange={handleInput} value={inputTo} placeholder='0' />
                     <select name="to" id="to" onChange={changeUnities} value={selectedTo}>
                     {arraySelected.map(el => {
                         return (
